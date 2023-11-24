333f55ce0f6d14c7c72df152816ff0947f7053a5
Lê Duy Hiếu <hieu24052002@gmail.com>
2023-11-24 14:47:46 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-document.querySelector('#avatarModel').addEventListener('model-loaded', function(evt){
-    let model = evt.detail.model;
-    let animations = model.animations;
-    console.log(animations);
-});
+
